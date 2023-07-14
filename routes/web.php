1c027c1cff18ac7fd61cc7cadf77f2d6f3729c12
alirfanyasin <risayunita21@gmail.com>
2023-07-14 20:54:42 +0700

    using log-viewer

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    Log::info('Ini adalah log info');
+});
 
 Route::get('/students', [StudentController::class, 'index']);
